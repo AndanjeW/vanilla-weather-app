926507ffeeed2146f7704d02b67c9fe4b8e61e9a
AndanjeW <94687120+AndanjeW@users.noreply.github.com>
2022-05-18 07:25:08 +0300

    updated current time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if(hours < 10){
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if(minutes < 10){
+        minutes = `0${minutes}`;
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response){
 let temperatureElement = document.querySelector("#current-temp");
 let cityElement = document.querySelector("#city");
@@ -6,12 +21,12 @@ let humidityElement = document.querySelector("#humidity");
 let dateElement = document.querySelector("#date")
 let conditionsElement = document.querySelector("#weather-conditions")
 
-console.log(response.data)
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML= response.data.name;
 windElement.innerHTML= Math.round(response.data.wind.speed);
 humidityElement.innerHTML= response.data.main.humidity;
 conditionsElement.innerHTML = response.data.weather[0].description;
+dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 
