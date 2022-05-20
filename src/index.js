bb8e6c61c44c0cc098ce1c13568c2c203f504b9c
AndanjeW <94687120+AndanjeW@users.noreply.github.com>
2022-05-20 08:02:10 +0300

    added event listeners to update unit conversions for Temp

--- src/index.js
+++ src/index.js
@@ -22,8 +22,10 @@ let dateElement = document.querySelector("#date")
 let conditionsElement = document.querySelector("#weather-conditions")
 let iconElement = document.querySelector("#icon")
 
+celsiusTemp = response.data.main.temp;
 
-temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+temperatureElement.innerHTML = Math.round(celsiusTemp);
 cityElement.innerHTML= response.data.name;
 windElement.innerHTML= Math.round(response.data.wind.speed);
 humidityElement.innerHTML= response.data.main.humidity;
@@ -50,4 +52,33 @@ function handleSearch(event){
 
 let form = document.querySelector("#search-form")
 form.addEventListener("submit", handleSearch);
+
+
+
+function showFahrenheitTemp(event){
+event.preventDefault();
+let temperatureElement = document.querySelector("#current-temp");
+celsiusLink.classList.remove("active");
+fahrenheitLink.classList.add("active");
+let fahrenheitTemp = (celsiusTemp * 9)/ 5 + 32;
+temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+
+}
+function showCelsiusTemp(event){
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#current-temp");
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    temperatureElement.innerHTML = Math.round(celsiusTemp);
+    
+    
+    }
+
+let celsiusTemp = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheitTemp)
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", showCelsiusTemp)
 search("Nairobi");
